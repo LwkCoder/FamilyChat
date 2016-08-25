0d18e4d9968d9aca78cf459215de3a6a89d26dd3
luowenkang <wenkang.luo@mytouchtime.com>
2016-08-25 09:43:46 +0800

    Finish dial feedback setting
    Change-Id: I670540f68c0d2e852098cecdb702ad299f54aceb

--- app/src/main/java/com/lwk/familycontact/project/setting/SettingActivity.java
+++ app/src/main/java/com/lwk/familycontact/project/setting/SettingActivity.java
@@ -1,16 +1,20 @@
 package com.lwk.familycontact.project.setting;
 
 import android.view.View;
+import android.widget.CompoundButton;
 
+import com.cengalabs.flatui.views.FlatToggleButton;
 import com.lib.base.widget.CommonActionBar;
 import com.lwk.familycontact.R;
 import com.lwk.familycontact.base.FCBaseActivity;
+import com.lwk.familycontact.storage.sp.SpSetting;
 
 /**
  * 设置界面
  */
-public class SettingActivity extends FCBaseActivity
+public class SettingActivity extends FCBaseActivity implements CompoundButton.OnCheckedChangeListener
 {
+    private FlatToggleButton mFtgDialFeedBack;
 
     @Override
     protected int setContentViewId()
@@ -24,11 +28,32 @@ public class SettingActivity extends FCBaseActivity
         CommonActionBar actionBar = findView(R.id.cab_setting);
         actionBar.setLeftLayoutAsBack(this);
         actionBar.setTitleText(R.string.tv_setting_title);
+
+        mFtgDialFeedBack = findView(R.id.ftg_setting_dial_feedback);
+        mFtgDialFeedBack.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    protected void initData()
+    {
+        super.initData();
+        mFtgDialFeedBack.setChecked(SpSetting.isDialFeedBackEnable(this));
     }
 
     @Override
     protected void onClick(int id, View v)
     {
 
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+    {
+        switch (buttonView.getId())
+        {
+            case R.id.ftg_setting_dial_feedback:
+                SpSetting.setDialFeendBackEnable(this, isChecked);
+                break;
+        }
     }
 }
